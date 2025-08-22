c2b96e0174e43cc7b31029c476500b18c7ecbbdd
Matti <mattinguyen12@gmail.com>
2025-08-21 17:08:17 -0700

    still fixing 12 stars formatting

--- utils.py
+++ utils.py
@@ -5,39 +5,48 @@ Formatting _mention_war_end embeded
 def formatting_member_stats(members_war_stats: list):
     twelve_stars = []
     six_stars = []
+    six_stars_townhall = []
     five_stars = []
     three_four_stars = []
     one_two_stars = []
-
-    double_accounts = [{"Justin": ["KingB", "kingblazen"],
-                        "Eishan": ["yolohoboswag", "*_yummy_*"]
-                        }]
-
-    for members in members_war_stats:
-        stars = int(members['members_total_stars'])
-        
-        # Format with code block for monospace alignment
-        formatted_line = f"\u202D{members['townhall']} `{members['members_name']:<13} ⭐{stars}  {members['members_total_attacks']}/2`\u202C"
-
-        for name, accounts in double_accounts:
-            sum_stars = sum(int(stars))
-            formatted_line_twelve_stars = f"\u202D{members['townhall']} `{name:<13} ⭐{sum_stars}  {members['members_total_attacks']}/2`\u202C"
-            if stars == 6 and accounts:
-                twelve_stars.append(formatted_line_twelve_stars)
-
-            elif stars == 6 :
-                six_stars.append(formatted_line)
-            elif stars == 5:
-                five_stars.append(formatted_line)
-            elif stars == 3 or stars == 4:
-                three_four_stars.append(formatted_line)
-            elif stars <= 2:
-                one_two_stars.append(formatted_line)
-
-    finalized_twelve_stars = "\n".join(twelve_stars) if twelve_stars else "None"
-    finalized_six_stars = "\n".join(six_stars) if six_stars else "None"
-    finalized_five_stars = "\n".join(five_stars) if five_stars else "None"
-    finalized_three_four_stars = "\n".join(three_four_stars) if three_four_stars else "None"
-    finalized_one_two_stars = "\n".join(one_two_stars) if one_two_stars else "None"
     
-    return finalized_twelve_stars, finalized_six_stars, finalized_five_stars, finalized_three_four_stars, finalized_one_two_stars
+    double_accounts = {"Justin":["KingB", "kingblazen"],
+                       "Eishan": ['yolohoboswag', '*_yummy_*']
+                       }
+    
+    #looks so nasty, make variables to make cleans cleaner.
+    #work in a mess, produce a mess XD
+    
+    for members in members_war_stats:
+        #looks so nasty, make variables to make cleans cleaner.
+        #work in a mess, produce a mess XD
+    
+        townhall = members['townhall'] 
+        members_name = members['members_name']
+        total_stars = members['members_total_stars']
+        total_attacks = members['members_total_attacks']
+        
+        if int(total_stars) == 6:
+            six_stars.append(members_name)
+            six_stars_townhall = townhall[0]
+            
+            for person, account_name in double_accounts.items():
+                if account_name in six_stars and account_name in double_accounts:
+                    twelve_stars.append(f"\u202D{six_stars_townhall} `{person:<12} \u200E⭐12 | 4/4`\u202D")                 
+                 
+        elif int(total_stars) == 5:
+            five_stars.append(f"\u202D{townhall} `{members_name:<12} \u200E⭐{total_stars} | {total_attacks}/2`\u202D")
+            
+        elif int(total_stars) == 3 or 4:
+            three_four_stars.append(f"\u202D{townhall} `{members_name:<12} \u200E⭐{total_stars} | {total_attacks}/2`\u202D")
+          
+        elif int(total_stars) <= 2:
+            one_two_stars.append(f"\u202D{townhall} `{members_name:<12} \u200E⭐{total_stars} | {total_attacks}/2`\u202D")
+        
+    fianlized_twelve_stars = "\n".join(twelve_stars) if twelve_stars else "`None! :(`"
+    finalized_six_stars = "\n".join(six_stars) if six_stars else "`None! :3`"
+    finalized_five_stars = "\n".join(five_stars) if five_stars else "`None! :3`"
+    finalized_three_four_stars = "\n".join(three_four_stars) if three_four_stars else "`None! :3`"
+    finalized_one_two_stars = "\n".join(one_two_stars) if one_two_stars else "`None! :3`"
+    
+    return fianlized_twelve_stars, finalized_six_stars, finalized_five_stars, finalized_three_four_stars, finalized_one_two_stars
